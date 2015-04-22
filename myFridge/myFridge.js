2fed3fd8669b26ac2496fd9c249ac24f1e8da5fb
Stephan Hochhaus <stephan@yauh.de>
2015-04-22 17:23:23 +0200

    remove unneeded code

--- myFridge/myFridge.js
+++ myFridge/myFridge.js
@@ -1,25 +1,9 @@
 Products = new Mongo.Collection('products');
 
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+
 }
